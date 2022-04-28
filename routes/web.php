0780237f7a7f0a84fdf408770ce97665775f6401
Chris Dawson <csdawson8@gmail.com>
2022-04-28 23:38:25 +0100

    Episode 2

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\FileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,6 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/files', [FileController::class, 'index'])->name('files');
 
 Route::middleware([
     'auth:sanctum',
