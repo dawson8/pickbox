eacb8a5e19b79693e0c3d8821236a50a6d38018f
Chris Dawson <csdawson8@gmail.com>
2022-04-29 01:18:45 +0100

    Episode 5

--- app/Models/Folder.php
+++ app/Models/Folder.php
@@ -4,8 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Folder extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name'
+    ];
+
+    public static function booted()
+    {
+        static::creating(function ($model) {
+            $model->uuid = Str::uuid();
+        });
+    }
 }
--- app/Models/Obj.php
+++ app/Models/Obj.php
@@ -4,10 +4,27 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Obj extends Model
 {
     use HasFactory;
 
     public $table = 'objects';
+
+    protected $fillable = [
+        'parent_id'
+    ];
+
+    public static function booted()
+    {
+        static::creating(function ($model) {
+            $model->uuid = Str::uuid();
+        });
+    }
+
+    public function objectable()
+    {
+        return $this->morphTo();
+    }
 }
